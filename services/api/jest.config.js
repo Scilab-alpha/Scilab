8650f931e2c133077bb88b6add54f2a28c80bd71
Bero <Bero.working@gmail.com>
2026-06-09 10:32:21 +0700

    remove .git

---
+++ services/api/jest.config.js
@@ -0,0 +1,10 @@
+// Source - https://stackoverflow.com/q/65105877
+// Posted by ProvingBard
+// Retrieved 2026-06-09, License - CC BY-SA 4.0
+
+module.exports = {
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
